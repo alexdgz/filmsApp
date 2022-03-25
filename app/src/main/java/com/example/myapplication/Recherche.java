ea617dcd098e940b12ab7b2e9cc8e02f63f0903e
Lucas <lucas.maillet44@gmail.com>
2022-03-25 11:12:31 +0100

    ratio ta vieille clé, c un token

---
+++ app/src/main/java/com/example/myapplication/Recherche.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recherche extends AppCompatActivity {
+
+
+    private String token = "92ab21b9b91fec38b3611c28cb06b710";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
