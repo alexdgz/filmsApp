1bed869f882b7bf1a116a85cb1c17a94167e6044
Lucas <lucas.maillet44@gmail.com>
2022-03-25 11:13:14 +0100

    ratio ta vieille clé, c un token

--- app/src/main/java/com/example/myapplication/Recherche.java
+++ app/src/main/java/com/example/myapplication/Recherche.java
@@ -9,7 +9,6 @@ public class Recherche extends AppCompatActivity {
 
     private String token = "92ab21b9b91fec38b3611c28cb06b710";
 
-    private String apiKey = "92ab21b9b91fec38b3611c28cb06b710";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
