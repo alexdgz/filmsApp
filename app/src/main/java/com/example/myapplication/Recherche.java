54b389d072dea296ce034f47c1b97c82b542a32e
Lucas <lucas.maillet44@gmail.com>
2022-03-25 12:09:11 +0100

    ok

--- app/src/main/java/com/example/myapplication/Recherche.java
+++ app/src/main/java/com/example/myapplication/Recherche.java
@@ -1,10 +1,18 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 
 public class Recherche extends AppCompatActivity {
@@ -13,6 +21,9 @@ public class Recherche extends AppCompatActivity {
     private String token = "92ab21b9b91fec38b3611c28cb06b710";
 
     private Button rechercher;
+    private EditText rechercheFilm;
+
+    private TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +32,29 @@ public class Recherche extends AppCompatActivity {
 
         rechercher = findViewById(R.id.searchBtnid);
 
+        rechercheFilm = findViewById(R.id.searchid);
+
+        testText = findViewById(R.id.textViewtest);
+
 
         rechercher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                Ion.with(view.getContext()).load("https://nosql-workshop.herokuapp.com/api/installations/search?query=" + commune.getText()).asJsonArray().setCallback(new FutureCallback<JsonArray>() {
+
+
+
+                Ion.with(view.getContext()).load("https://api.themoviedb.org/3/search/movie?api_key="+token+"&language=fr&query=" +rechercheFilm.getText()+"&page=1&include_adult=false").asJsonObject().setCallback(new FutureCallback<JsonObject>() {
                     @Override
-                    public void onCompleted(Exception e, JsonArray result) {
-                        nomActivite.setText(String.valueOf(result));
+                    public void onCompleted(Exception e, JsonObject result) {
+
+                    Intent resultatActivity = new Intent(getApplicationContext(), Resultats.class);
+
+                        resultatActivity.putExtra("resultat", String.valueOf(result));
+
+                        startActivity(resultatActivity);
+
                     }
                 });
 
--- app/src/main/java/com/example/myapplication/Resultats.java
+++ app/src/main/java/com/example/myapplication/Resultats.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.JsonObject;
 
 public class Resultats extends AppCompatActivity {
 
+    private TextView test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultat);
+        test = findViewById(R.id.textViewtest);
+
+        JsonObject resultats = getIntent().getParcelableExtra("resultats");
+
+      //  test.setText(String.valueOf(resultats));
+
+
+
     }
 }
