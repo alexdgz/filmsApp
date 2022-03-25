3734c162583d350a5b064b3b51b158bb215c7fb0
Lucas <lucas.maillet44@gmail.com>
2022-03-25 11:28:36 +0100

    ion c de la d

--- app/src/main/java/com/example/myapplication/Recherche.java
+++ app/src/main/java/com/example/myapplication/Recherche.java
@@ -1,17 +1,44 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class Recherche extends AppCompatActivity {
 
 
     private String token = "92ab21b9b91fec38b3611c28cb06b710";
 
+    private Button rechercher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recherche);
+
+        rechercher = findViewById(R.id.searchBtnid);
+
+
+        rechercher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Ion.with(view.getContext()).load("https://nosql-workshop.herokuapp.com/api/installations/search?query=" + commune.getText()).asJsonArray().setCallback(new FutureCallback<JsonArray>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonArray result) {
+                        nomActivite.setText(String.valueOf(result));
+                    }
+                });
+
+            }
+        });
+
+
+
+
     }
 }
